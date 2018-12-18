f406f1d05ccc457d53795203e2cd57ec87c72f8b
Trey Work <trey.wayne@trusttiger.com>
2018-12-18 16:12:00 -0600

    starting migrating to using API

--- servicetitan.py
+++ servicetitan.py
@@ -2,36 +2,46 @@ import requests
 import json
 import datetime
 
-headers = {"Accept": "application/json", "serviceTitanApiKey" : "8ae6480a-2499-4e84-bd09-7c461f033ddb", "startsAfter" : "2018-11-11", "pageSize" : "20000"}
-resp = requests.get('HTTPS://api.servicetitan.com/v1/jobs?', params = headers)
-
-with open("jobsdump.json", "w") as write_file:
-    json.dump(resp.json(), write_file)
 
 
-with open("jobsdump.json", "r") as read_file:
-    data = json.load(read_file)
+def getjobs(**kwargs):
+    essential = {"Accept": "application/json", "serviceTitanApiKey" : "8ae6480a-2499-4e84-bd09-7c461f033ddb", "pageSize" : "200000"}
+    headers = {**kwargs, **essential}
+    resp = requests.get('HTTPS://api.servicetitan.com/v1/jobs?', params = headers)
+
+    with open("jobsdump.json", "w") as write_file:
+        json.dump(resp.json(), write_file)
 
 
-jobs = data['data']
-total = 0.0
-print(type(jobs))
-for x in range(len(jobs)):
-    jobs[x] = dict(jobs[x])
-    jobs[x]['businessUnit'] = dict(jobs[x]['businessUnit'])
+    with open("jobsdump.json", "r") as read_file:
+        data = json.load(read_file)
+    return data['data']
 
-    if(jobs[x]['invoice'] != None):
-        jobs[x]['invoice'] = dict(jobs[x]['invoice'])
 
-        
-        testdate = datetime.date(2018, 11, 11)
-        jobstart = datetime.datetime.strptime(jobs[x]['start'][:19], "%Y-%m-%dT%H:%M:%S").date()
-        # if(jobstart >= testdate):
-        print (jobs[x]['businessUnit']['name'])
-        # print (jobs[x]['businessUnit']['id'])
-        #print(jobs[x]['invoice']['total'])
-        #total += jobs[x]['invoice']['total']
-        for y in range(len(jobs[x]['tags'])):
-            jobs[x]['tags'][y] = dict(jobs[x]['tags'][y])
-            print(jobs[x]['tags'][y]['name'])
-        #print(total)
+# total = 0.0
+# print(type(jobs))
+# for x in range(len(jobs)):
+#     jobs[x] = dict(jobs[x])
+#     jobs[x]['businessUnit'] = dict(jobs[x]['businessUnit'])
+
+#     if(jobs[x]['invoice'] != None):
+#         jobs[x]['invoice'] = dict(jobs[x]['invoice'])
+
+#         testdate = datetime.date(2018, 11, 11)
+#         jobstart = datetime.datetime.strptime(jobs[x]['start'][:19], "%Y-%m-%dT%H:%M:%S").date()
+#         print (jobs[x]['businessUnit']['name'])
+#         for y in range(len(jobs[x]['tags'])):
+#             jobs[x]['tags'][y] = dict(jobs[x]['tags'][y])
+#             print(jobs[x]['tags'][y]['name'])
+
+def main():
+    jobs = getjobs(completedAfter='2018-01-01')
+    for x in range(len(jobs)):
+        jobs[x] = dict(jobs[x])
+        jobs[x]['businessUnit'] = dict(jobs[x]['businessUnit'])
+        if(jobs[x]['invoice'] != None):
+            jobs[x]['invoice'] = dict(jobs[x]['invoice'])
+
+
+if __name__ == "__main__":
+    main()
